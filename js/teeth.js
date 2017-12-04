f77343deef2215e7b9c56b6f4804612c7e3d24c3
DESKTOP-QQJ63PQ\tambu <kontakt@lukaszwozny.com>
2017-12-04 22:22:57 +0100

    Move one tooth to correct place

--- js/teeth.js
+++ js/teeth.js
@@ -5,35 +5,39 @@ function changeColor()
 
 function testownia()
 {
-    var ctx = document.createElement("canvas").getContext("2d");
+    var canvas = document.createElement("canvas");
+
+    var ctx = canvas.getContext("2d");
     
+    
+    document.getElementById("tooth").style.top = 364+"px";
+    document.getElementById("tooth").style.left = 63+"px";
     $('#tooth').on("mousedown", function(event) {
+        // Get click coordinates
+        var x = event.pageX - this.offsetLeft,
+            y = event.pageY - this.offsetTop,
+            w = ctx.canvas.width = this.width,
+            h = ctx.canvas.height = this.height,
+            alpha;
 
-      // Get click coordinates
-      var x = event.pageX - this.offsetLeft,
-          y = event.pageY - this.offsetTop,
-          w = ctx.canvas.width = this.width,
-          h = ctx.canvas.height = this.height,
-          alpha;
+        // Draw image to canvas
+        // and read Alpha channel value
+        ctx.drawImage(this, 0, 0, w, h);
+        alpha = ctx.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
 
-      // Draw image to canvas
-      // and read Alpha channel value
-      ctx.drawImage(this, 0, 0, w, h);
-      alpha = ctx.getImageData(x, y, 1, 1).data[3]; // [0]R [1]G [2]B [3]A
-
-      // If pixel is transparent,
-      // retrieve the element underneath and trigger it's click event
-      if( alpha===0 ) {
-        $(this).hide();
-        $(document.elementFromPoint(event.clientX, event.clientY)).trigger("click");
-        $(this).show();
-      } else {
-        console.log("LOGO clicked!");
-      }
+          // If pixel is transparent,
+          // retrieve the element underneath and trigger it's click event
+        if( alpha===0 ) {
+            $(this).hide();
+            $(document.elementFromPoint(event.clientX, event.clientY)).trigger("click");
+            $(this).show();
+        } else {
+            console.log("LOGO clicked!");
+        }
     });
     
     $("#teeth-map").on("click", function(){
-      console.log("Green image clicked!");
+        console.log("Green image clicked!");
     });
 }
 
