8a4acb665421d6aad0840bd41456a8e39fb03e9f
DESKTOP-QQJ63PQ\tambu <kontakt@lukaszwozny.com>
2017-12-05 16:16:16 +0100

    Add whole new Tooth

--- js/teeth-svg.js
+++ js/teeth-svg.js
@@ -1,7 +1,56 @@
 var COORDS = new Array();
+var COORDS_2 = new Array();
 var w1 = 100;
 var h1 = 25;
 COORDS[1] = '0,0 '+w1+',0 '+(w1-h1)+','+h1+' '+h1+','+h1;
+
+COORDS_2[1] = [
+    [0,0], [100,0],
+    [30,30], [70,30],
+    [30,70], [70,70],
+    [0,100], [100,100]
+];
+
+TOP = [0,1,3,2];
+LEFT = [0,2,4,6];
+RIGHT = [1,3,5,7];
+BOTTOM = [6,4,5,7];
+CENTER = [2,3,5,4];
+
+function buildCoords(tab, points)
+{
+    var coords ='';
+    for(i=0; i<points.length; i++){
+        var p = points[i];
+        coords += tab[p][0]+','+tab[p][1]+' ' 
+    }
+    return coords;
+}
+
+function buildPolygon(draw, tab, side)
+{
+    var points = '';
+    switch(side) {
+        case "TOP":
+            points = TOP;
+            break;
+        case "RIGHT":
+            points = RIGHT;
+            break;
+        case "BOTTOM":
+            points = BOTTOM;
+            break;
+        case "LEFT":
+            points = LEFT;
+            break;
+        case "CENTER":
+            points = CENTER;
+            break;
+    }
+    var coords = buildCoords(tab, points);
+    var polygon = draw.polygon(coords);
+    return polygon;
+}
 
 function Tooth(id, posX, posY, size){
     this.id = id;
@@ -14,6 +63,29 @@ function Tooth(id, posX, posY, size){
     this.bottom = "";
     this.left = "";
     this.center = "";
+    
+    this.group = "";
+}
+
+Tooth.prototype.build_tooth = function(draw){
+    this.top = buildPolygon(draw, COORDS_2[this.size], "TOP");
+    this.right = buildPolygon(draw, COORDS_2[this.size], "RIGHT");
+    this.bottom = buildPolygon(draw, COORDS_2[this.size], "BOTTOM");
+    this.left = buildPolygon(draw, COORDS_2[this.size], "LEFT");
+    this.center = buildPolygon(draw, COORDS_2[this.size], "CENTER");
+    
+    this.group = draw.group();
+    this.group.add(this.top);
+    this.group.add(this.right);
+    this.group.add(this.bottom);
+    this.group.add(this.left);
+    this.group.add(this.center);
+    this.group.attr({
+        fill: '#fff',
+        'fill-opacity': 0.5,
+        stroke: '#000',
+        'stroke-width': 1
+    })
 }
 
 Tooth.prototype.draw = function(svg){
@@ -39,7 +111,7 @@ Tooth.prototype.draw = function(svg){
     polygon = svg.polygon(COORDS[this.size])
     polygon.fill('#906').move(x+dx, y+dy);
     polygon.transform({ rotation: 90 });
-    this.righ = polygon;
+    this.right = polygon;
     
     // bottom
     dx = 0;
@@ -67,8 +139,17 @@ Tooth.prototype.draw = function(svg){
     polygon = svg.polygon(coords)
     polygon.fill('#ccc').move(x+dx, y+dy);
     polygon.transform({ rotation: -90 });
+    this.center = polygon;
     
-    this.center = "";
+    this.group =svg.group();
+    this.group.add(this.top);
+    this.group.add(this.right);
+    this.group.add(this.bottom);
+    this.group.add(this.left);
+    this.group.add(this.center);
+    
+    this.group.move(40,90).scale(0.9);
+    
 };
 
 
@@ -80,7 +161,10 @@ function testownia()
     var draw = SVG('drawing').size(width, height)
     
     var t = new Tooth(1, 20, 40, 1);
-    t.draw(draw);
+//    t.draw(draw);
+    t.build_tooth(draw);
+    
+//    buildPolygon(draw, COORDS_2[1], 'CENTER');
     
 //    var text = draw.text('I know that eggs do well to stay out of frying pans.')
 //    text.move(20,20).font({ fill: '#f06', family: 'Inconsolata' })
