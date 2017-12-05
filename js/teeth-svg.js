d01c589f32a6f452fa2cfab7cd34ece80c9078ae
DESKTOP-QQJ63PQ\tambu <kontakt@lukaszwozny.com>
2017-12-05 13:10:30 +0100

    Init new html

---
+++ js/teeth-svg.js
@@ -0,0 +1,10 @@
+function testownia()
+{
+    
+    console.log('Hello');
+//    $("#teeth-map").on("click", function(){
+//        console.log("Green image clicked!");
+//    });
+}
+
+window.onload = testownia
